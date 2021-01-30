b8f3ab40f869f826058a6b07ae0e73cc73c6d4d0
ackermZed <790863544@qq.com>
2021-01-30 19:31:24 +0800

    Week03HomeWorkForNetty

---
+++ Week_03/HomeWorkForNetty/src/main/java/io/zed/gateway/outbound/httpclient4/HttpOutboundHandler.java
@@ -0,0 +1,150 @@
+package io.zed.gateway.outbound.httpclient4;
+
+
+import io.zed.gateway.filter.HttpRequestFilter;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import io.zed.gateway.router.HttpEndpointRouter;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+import static org.apache.http.HttpHeaders.CONNECTION;
+
+public class HttpOutboundHandler {
+
+    private CloseableHttpAsyncClient httpclient;
+    private ExecutorService proxyService;
+    private String backendUrl;
+    private HttpRequestFilter filter;
+
+    public HttpOutboundHandler(List<String> backendUrls, HttpRequestFilter filter) {
+        // 路由
+        HttpEndpointRouter router = new HttpEndpointRouter() {
+            @Override
+            public String route(List<String> urls) {
+                int i = new Random().nextInt(2);
+                System.out.println(i);
+                return urls.get(i);
+            }
+        };
+        String url = router.route(backendUrls);
+        this.backendUrl = url.endsWith("/") ? url.substring(0, url.length() - 1) : url;
+        int cores = Runtime.getRuntime().availableProcessors() * 2;
+        long keepAliveTime = 1000;
+        int queueSize = 2048;
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.CallerRunsPolicy();//.DiscardPolicy();
+        proxyService = new ThreadPoolExecutor(cores, cores,
+                keepAliveTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(queueSize),
+                new NamedThreadFactory("proxyService"), handler);
+
+        IOReactorConfig ioConfig = IOReactorConfig.custom()
+                .setConnectTimeout(1000)
+                .setSoTimeout(1000)
+                .setIoThreadCount(cores)
+                .setRcvBufSize(32 * 1024)
+                .build();
+
+        httpclient = HttpAsyncClients.custom().setMaxConnTotal(40)
+                .setMaxConnPerRoute(8)
+                .setDefaultIOReactorConfig(ioConfig)
+                .setKeepAliveStrategy((response, context) -> 6000)
+                .build();
+        httpclient.start();
+    }
+
+    public void handle(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, HttpRequestFilter filter) {
+        final String url = this.backendUrl + fullRequest.uri();
+        filter.filter(fullRequest, ctx);
+        proxyService.submit(() -> fetchGet(fullRequest, ctx, url));
+    }
+
+    private void fetchGet(final FullHttpRequest inbound, final ChannelHandlerContext ctx, final String url) {
+        final HttpGet httpGet = new HttpGet(url);
+        //httpGet.setHeader(HTTP.CONN_DIRECTIVE, HTTP.CONN_CLOSE);
+        httpGet.setHeader(HTTP.CONN_DIRECTIVE, HTTP.CONN_KEEP_ALIVE);
+        httpGet.setHeader("httpGetHeader", inbound.headers().get("StrongerHeaderByNetty"));
+        httpclient.execute(httpGet, new FutureCallback<HttpResponse>() {
+            @Override
+            public void completed(final HttpResponse endpointResponse) {
+                try {
+                    handleResponse(inbound, ctx, endpointResponse);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+
+                }
+            }
+
+            @Override
+            public void failed(final Exception ex) {
+                httpGet.abort();
+                ex.printStackTrace();
+            }
+
+            @Override
+            public void cancelled() {
+                httpGet.abort();
+            }
+        });
+    }
+
+    private void handleResponse(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, HttpResponse endpointResponse) throws Exception {
+        FullHttpResponse response = null;
+        try {
+
+            // response增强
+            // byte[] body = EntityUtils.toByteArray(endpointResponse.getEntity());
+            String value = EntityUtils.toString(endpointResponse.getEntity(), "utf-8");
+            value = value + " and Stronged by Netty!!!";
+
+            byte[] body = value.getBytes("utf-8");
+
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", body.length);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+            exceptionCaught(ctx, e);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+            ctx.flush();
+        }
+
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
---
+++ Week_03/HomeWorkForNetty/src/main/java/io/zed/gateway/outbound/okhttp/OkhttpOutboundHandler.java
@@ -0,0 +1,4 @@
+package io.zed.gateway.outbound.okhttp;
+
+public class OkhttpOutboundHandler {
+}
